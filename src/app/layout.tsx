902c5e7a09c8e91ffdc185e6414d5e142a0aa59f
RupeshVd <rupesh.kumar@vedam.org>
2025-07-01 23:56:42 +0530

    VF-69 Footer added Lint fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Outfit } from 'next/font/google';
 
 import './globals.css';
-import UTMCaptureClient from '../components/UTMCaptureClient';
 import Footer from '@/components/Footer';
+
+import UTMCaptureClient from '../components/UTMCaptureClient';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
