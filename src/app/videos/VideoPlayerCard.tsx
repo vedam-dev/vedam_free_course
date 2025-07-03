ec837c57b48aa4bc2a9e5d6f1b3ebe7859488dbc
HRBvedam <hrishabh.bharati@vedam.org>
2025-07-03 10:51:35 +0530

    VF-79m = Vedam FavIcon Updated

--- src/app/videos/VideoPlayerCard.tsx
+++ src/app/videos/VideoPlayerCard.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material';
+import Image from 'next/image';
 import React from 'react';
 
 interface VideoPlayerCardProps {
@@ -6,31 +7,72 @@ interface VideoPlayerCardProps {
 }
 
 const VideoPlayerCard: React.FC<VideoPlayerCardProps> = ({ shortcode }) => {
+  const sourceType = process.env.NEXT_PUBLIC_GDRIVE;
+  const src =
+    sourceType === 'google'
+      ? `https://drive.google.com/file/d/${shortcode}/preview`
+      : `https://streamable.com/e/${shortcode}`;
 
   return (
     <Box
       sx={{
         position: 'relative',
-        paddingTop: '56.25%',
+        paddingTop: '56.25%', // 16:9
         width: '100%',
-        borderRadius: '8px',
+        borderRadius: 2,
         overflow: 'hidden',
+        backgroundColor: 'black',
       }}
     >
       <iframe
-        width="100%"
-        height="100%"
-        src={`https://streamable.com/o/${shortcode}`}
+        src={src}
+        title="Video player"
         allow="accelerometer; autoplay; clipboard-write; gyroscope; picture-in-picture; web-share"
-        title="YouTube video player"
         allowFullScreen
         style={{
           position: 'absolute',
           top: 0,
           left: 0,
+          width: '100%',
+          height: '100%',
           border: 'none',
+          zIndex: 1,
         }}
       />
+
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 12,
+          right: 12,
+          width: 40,
+          height: 40,
+          zIndex: 2,
+          pointerEvents: 'auto',
+          background: 'transparent',
+        }}
+      />
+
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 12,
+          right: 12,
+          width: 40,
+          height: 40,
+          zIndex: 3,
+          pointerEvents: 'none',
+          background: 'white',
+        }}
+      >
+        <Image
+          src="/home/videoInfo/VedamLogo.png"
+          alt="Vedam Logo"
+          fill
+          style={{ objectFit: 'contain' }}
+          priority
+        />
+      </Box>
     </Box>
   );
 };
