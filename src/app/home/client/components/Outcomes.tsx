dd65b6c347b71938501cdb09a1b65b64196a3211
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-07-05 20:14:10 +0530

    VF-95 supabase storage implemented (#66)

--- src/app/home/client/components/Outcomes.tsx
+++ src/app/home/client/components/Outcomes.tsx
@@ -105,7 +105,7 @@ const Outcomes = () => {
               >
                 {item.featured && (
                   <Image
-                    src="/home/star.png"
+                    src="https://acjlsquedaotbhbxmtee.supabase.co/storage/v1/object/public/vedam-website-assets/images/others/star.png"
                     alt="Featured"
                     width={isMobile ? 65 : 100}
                     height={isMobile ? 65 : 100}
