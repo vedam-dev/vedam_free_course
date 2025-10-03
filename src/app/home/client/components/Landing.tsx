00e879ef447895742521f37e42e8c26f715a5649
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-10-03 13:39:42 +0530

    VF-323 fixed redirection on click of Start Now

--- src/app/home/client/components/Landing.tsx
+++ src/app/home/client/components/Landing.tsx
@@ -7,7 +7,7 @@ import {
   Divider,
   Typography,
 } from '@mui/material';
-import { useRouter } from 'next/navigation';
+
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -39,7 +39,6 @@ function stringToColor(str: string) {
 }
 
 const Landing: React.FC = () => {
-  const router = useRouter();
   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
   const username = useSelector((state: RootState) => state.user.username);
   const [hasMounted, setHasMounted] = useState(false);
@@ -51,7 +50,10 @@ const Landing: React.FC = () => {
       setShowOtpModal(true);
       return;
     } else {
-      router.push('/videos');
+      const element = document.getElementById('VideoCourses');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
     }
   };
 
