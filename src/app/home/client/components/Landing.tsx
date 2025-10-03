8b23c559c62e911140add0af91603f306de6eb01
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-10-03 10:04:11 +0530

    VF-324 fixed post login button

--- src/app/home/client/components/Landing.tsx
+++ src/app/home/client/components/Landing.tsx
@@ -11,6 +11,7 @@ import {
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation';
 
 import BaseButton from '@/components/BaseButton';
 import BaseDecoration from '@/components/BaseDecoration';
@@ -40,6 +41,7 @@ function stringToColor(str: string) {
 }
 
 const Landing: React.FC = () => {
+  const router = useRouter();
   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
   const username = useSelector((state: RootState) => state.user.username);
   const [hasMounted, setHasMounted] = useState(false);
@@ -53,10 +55,7 @@ const Landing: React.FC = () => {
       setShowOtpModal(true);
       return;
     } else {
-      setSnackbarMessage(
-        `Welcome back, ${username}!, You are already Logged In`
-      );
-      setSnackbarOpen(true);
+      router.push('/videos');
     }
   };
 
@@ -308,7 +307,7 @@ const Landing: React.FC = () => {
                   background: 'white',
                 }}
               >
-                Register for Codesprint
+                {isLoggedIn ? 'Start Now' : 'Register for Codesprint'}
               </BaseButton>
               {/* <BaseButton variant="contained" size="large">
                 Register for later
