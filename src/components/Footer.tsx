2fbf342017c35d6c29f120f3336ef1c183973d53
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-10-03 10:25:01 +0530

    VF-324 updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,10 +27,10 @@ const FOOTER_LINKS: FooterColumn[] = [
   {
     title: 'Quick Links',
     links: [
+      { label : 'Visit our website', href: 'https://vedam.org/' },
       { label: 'Terms of use', href: 'https://vedam.org/terms-of-use' },
       { label: 'Privacy Policy', href: 'https://vedam.org/privacy-policy' },
       { label: 'Refund Policy', href: 'https://vedam.org/refund-policy' },
-      { label : 'Visit our website', href: 'https://vedam.org/' }
     ],
   },
   {
