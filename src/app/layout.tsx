306d81e2bfcd511b531af484dcd1297662adc7ae
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-10-03 11:26:24 +0530

    VF-325 added floating button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Geist, Geist_Mono, Outfit } from 'next/font/google';
 import { Provider } from 'react-redux';
+import FloatingButton from '@/components/FloatingButton';
 
 
 import './globals.css';
@@ -40,6 +41,7 @@ export default function RootLayout({
           <UTMCaptureClient>
             <ClientGaurd>
               {children}
+              <FloatingButton />
             </ClientGaurd>
             <Footer />
           </UTMCaptureClient>
