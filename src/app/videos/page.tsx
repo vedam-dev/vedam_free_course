3b9a39b0ac10a3c9631736905ea072fe668ec16b
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-10-08 18:39:27 +0530

    VF-336 about us page fixed

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -3,12 +3,15 @@ import { Suspense } from 'react';
 
 import Loading from '@/components/Loading';
 
+import AboutUs from '../home/client/components/AboutUs';
+
 import VideoWatchPageContent from './VideoWatchPageContent';
 
 export default function VideosPage() {
   return (
     <Suspense fallback={<Loading />}>
       <VideoWatchPageContent />
+      <AboutUs/>
     </Suspense>
   );
 }
