704aa8d26ee5683fc9da55af58a384ccf05e19a6
Praz-Vedam <prasanna.swain@vedam.org>
2025-12-03 12:39:21 +0530

    VF-458 Added meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,13 +35,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2046732509398744');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID_HERE&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${outfit.className}`}>
         <Provider store={store}>
           <UTMCaptureClient>
             <ClientGaurd>
               {children}
               <FloatingButton />
-              <BannerPopupModal/>
+              <BannerPopupModal />
             </ClientGaurd>
             <Footer />
           </UTMCaptureClient>
