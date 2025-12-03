1f0e59c36cfbfa2243428b008476dd69bd117404
Praz-Vedam <prasanna.swain@vedam.org>
2025-12-03 12:43:47 +0530

    VF-458 Added google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K7ZDF4K4');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* Meta Pixel */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -53,15 +68,28 @@ export default function RootLayout({
           }}
         />
         <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID_HERE&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2046732509398744&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${outfit.className}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K7ZDF4K4"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Provider store={store}>
           <UTMCaptureClient>
             <ClientGaurd>
