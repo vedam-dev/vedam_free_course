b8f494916292f3cb64fec6efb242172907248983
Praz-Vedam <prasanna.swain@vedam.org>
2026-01-08 10:28:25 +0530

    VF-481 Gtag removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,12 @@
 'use client';
 import { Box } from '@mui/material';
-import Script from 'next/script';
-
-
 
 import HomePage from './home/client/HomePage';
 
 export default function HomePageComponent() {
-
-
   return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-KC46RHD2WJ"
-      />
-
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-KC46RHD2WJ');
-          `,
-        }}
-      />
-
-
-      <Box sx={{ textAlign: 'center' }}>
-        <HomePage />
-      </Box>
-    </>
+    <Box sx={{ textAlign: 'center' }}>
+      <HomePage />
+    </Box>
   );
 }
